4d7c7b74c2aa09c86082b956d529b3f51a5175a8
Bram Moreinis <bmoreinis@gmail.com>
2021-06-15 18:14:01 +0000

    added special stuff

--- script.js
+++ script.js
@@ -5,13 +5,13 @@
 var trigger = "quit";
 var answer = ""; 
 var cpuTalk = ["hi","yes","no","How are you?","lets play a game!","Rainy day today.","whats your name","I have a joke.  Want to hear it?","Do I have a cicada on my back?","How is your day going?","Want to listen to music","whats your favorite movie","S'up!","Hey","What\'s your favorite movie?","Looking forward to summer?","Are shadows made of matter?","What\'s your favorite book?","What\'s your favorite color?","What\'s your favorite ice cream flavor?","What\'s your favorite animal?","What\'s you favorite cereal?", "What grade are you in?"];
-var userTalk = ["favorite movie","grade", "how are you"];
+var userTalk = ["favorite movie","favorite color","grade", "how are you","mine is"];
 var promptInt = 0;
 var cpuSay = cpuTalk[promptInt];
 
 while(answer != trigger){
   answer = prompt(cpuSay);
-  stringCheck = stringChecker(answer.toLowerCase());
+  let stringCheck = stringChecker(answer.toLowerCase());
   if (answer == trigger) {
     alert("Nice talkin' to ya!");
   }
@@ -37,15 +37,21 @@ function stringChecker(answer){
   return responseIndex;
 }
 
-function cpuThink(answer){
+function cpuThink(answerPart){
   var response = "You don\'t say!";
-  if (answer == "favorite movie") {
+  if (answerPart == "favorite movie") {
      response = "Avengers Endgame";
   }
-  else if (answer == "grade"){
+  else if (answerPart == "favorite color") {
+     response = "purple";
+  }
+  else if (answerPart == "mine is"){
+    response = lastPhrase(answer, "mine is");
+  }
+  else if (answerPart == "grade"){
     response = "I\'m not in school anymore.";
   }
-  else if (answer == "how are you") {
+  else if (answerPart == "how are you") {
     response = queTal();
   }
   return response; 
@@ -53,6 +59,13 @@ function cpuThink(answer){
 
 function queTal(){
   var posibilidades = ["Bien aqui volando a la luna!","Bien, estoy buscando hormigas para mi coleccion.","Bien, buscando hormigas para comer."];
-  hoy = Math.floor(Math.random()*posibilidades.length);
+  let hoy = Math.floor(Math.random()*posibilidades.length);
   return posibilidades[hoy];
+}
+
+function lastPhrase(answer,whatThing){
+  let cutHowMany = whatThing.split(" ").length;
+  let answerArray = answer.split(" ");
+  let response = answerArray.slice(cutHowMany);
+  return response.join(" ")+" is one of my faves, too!";
 }
